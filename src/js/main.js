7a28b509641706dc951b0e358025843bba5af741
vmik7 <vmik7@yandex.ru>
2021-06-23 17:32:23 +0300

    Адаптив шапки

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,28 @@
-const menuList = document.querySelector('.menu__list');
+const menu = document.querySelector('.menu');
+const menuBurger = document.querySelector('.menu__burger');
+const menuCloseButton = document.querySelector('.menu__close');
+const menuBody = document.querySelector('.menu__body');
 const searchInput = document.querySelector('.search__input');
+const searchShowButton = document.querySelector('.menu__show-search');
 
 searchInput.addEventListener('focus', () => {
-    menuList.classList.toggle('menu__list_hidden', true);
+    menu.classList.toggle('menu_state_search', true);
 });
 searchInput.addEventListener('blur', () => {
-    menuList.classList.toggle('menu__list_hidden', false);
+    menu.classList.toggle('menu_state_search', false);
+});
+searchShowButton.addEventListener('click', () => {
+    searchInput.focus();
+});
+
+menuBurger.addEventListener('click', () => {
+    menu.classList.toggle('menu_expanded', true);
+});
+menuCloseButton.addEventListener('click', () => {
+    menu.classList.toggle('menu_expanded', false);
+});
+menuBody.addEventListener('click', (e) => {
+    if (e.target === menuBody) {
+        menu.classList.toggle('menu_expanded', false);
+    }
 });
