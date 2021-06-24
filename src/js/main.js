edbe55067992dc97aae36efbc66057e2ae5eb8f6
vmik7 <vmik7@yandex.ru>
2021-06-24 14:27:18 +0300

    Заготовки контента, swiper

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,17 @@
+/* eslint-disable spaced-comment */
+
+//=include scripts/sliders.js
+
+/* eslint-enable spaced-comment */
+
+const page = document.querySelector('.page');
 const menu = document.querySelector('.menu');
 const menuBurger = document.querySelector('.menu__burger');
 const menuCloseButton = document.querySelector('.menu__close');
 const menuBody = document.querySelector('.menu__body');
 const searchInput = document.querySelector('.search__input');
 const searchShowButton = document.querySelector('.menu__show-search');
+const heroSlider = document.querySelector('.hero-slider');
 
 searchInput.addEventListener('focus', () => {
     menu.classList.toggle('menu_state_search', true);
@@ -17,12 +25,23 @@ searchShowButton.addEventListener('click', () => {
 
 menuBurger.addEventListener('click', () => {
     menu.classList.toggle('menu_expanded', true);
+    page.classList.toggle('page_locked', true);
 });
 menuCloseButton.addEventListener('click', () => {
     menu.classList.toggle('menu_expanded', false);
+    page.classList.toggle('page_locked', false);
 });
 menuBody.addEventListener('click', (e) => {
     if (e.target === menuBody) {
         menu.classList.toggle('menu_expanded', false);
+        page.classList.toggle('page_locked', false);
     }
 });
+
+if (heroSlider) {
+    /* eslint-disable-next-line no-undef */
+    const swiper = new Swiper(heroSlider, {
+        speed: 400,
+        spaceBetween: 100,
+    });
+}
