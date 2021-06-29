64faf6d58ebb12cbf3d98e9e3565cccc9760b90f
vmik7 <vmik7@yandex.ru>
2021-06-29 23:24:31 +0300

    Fix deploy script to stash changes before deploy

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -4,8 +4,11 @@ const { buildFolderName } = require('../gulp/routes');
 
 (async () => {
     try {
+        console.log('1. Stashing changes ...');
+        await execFile('git', ['stash', 'save', '-u', '"stash before deploy"']);
+
+        console.log('2. Building ...');
         await execFile('git', ['checkout', '--orphan', 'gh-pages']);
-        console.log('Building...');
         await execFile('npm', ['run', 'build']);
         await execFile('git', ['--work-tree', buildFolderName, 'add', '--all']);
         await execFile('git', [
@@ -15,14 +18,22 @@ const { buildFolderName } = require('../gulp/routes');
             '-m',
             'gh-pages',
         ]);
-        console.log('Pushing to gh-pages...');
+
+        console.log('3. Pushing to gh-pages ...');
         await execFile('git', ['push', 'origin', 'HEAD:gh-pages', '--force']);
         await execFile('rm', ['-r', buildFolderName]);
         await execFile('git', ['checkout', '-f', 'master']);
         await execFile('git', ['branch', '-D', 'gh-pages']);
+
+        console.log('4. Applying stashed changes ...');
+        await execFile('git', ['stash', 'apply']);
+
         console.log('Successfully deployed!');
     } catch (e) {
+        console.error('Have some errors!');
+
         console.log(e.message);
+
         process.exit(1);
     }
 })();
